f90deb36e957c3f0db43213387b013a23bceedfc
wadakenji <ponporopen@gmail.com>
2024-06-22 13:22:46 +0900

    feat: FieldSetに外からスタイルをあてられるように

--- src/components/FieldSet/index.tsx
+++ src/components/FieldSet/index.tsx
@@ -1,13 +1,19 @@
 import { FC, PropsWithChildren } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 type Props = {
   label: string;
+  className?: string;
 };
 
-const FieldSet: FC<PropsWithChildren<Props>> = ({ label, children }) => {
+const FieldSet: FC<PropsWithChildren<Props>> = ({
+  label,
+  className,
+  children,
+}) => {
   return (
     <>
-      <label className="max-w-full w-60">
+      <label className={twMerge('max-w-full', className)}>
         <span className="block font-bold text-lg mb-2">{label}</span>
         {children}
       </label>
