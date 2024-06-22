25c4bf776173a76028cc0ffbac1083ca015e9f25
wadakenji <ponporopen@gmail.com>
2024-06-22 13:21:32 +0900

    feat: InputButtonSetコンポーネント作成

---
+++ src/components/InputButtonSet/index.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+import NumberInput from '../NumberInput';
+import { twMerge } from 'tailwind-merge';
+
+type Props = {
+  value: number;
+  onChange: (value: number) => void;
+  className?: string;
+};
+
+const InputButtonSet: FC<Props> = ({ value, onChange, className }) => {
+  const increment = () => onChange(value + 1);
+  const decrement = () => onChange(value - 1);
+
+  return (
+    <>
+      <span className={twMerge('flex gap-x-2 items-center', className)}>
+        <button
+          className="w-8 h-8 border border-gray-300 bg-white rounded-full"
+          onClick={decrement}
+        >
+          -
+        </button>
+        <NumberInput
+          className="w-10 text-lg font-bold"
+          value={value}
+          onChange={onChange}
+        />
+        <button
+          className="w-8 h-8 border border-gray-300 bg-white rounded-full"
+          onClick={increment}
+        >
+          +
+        </button>
+      </span>
+    </>
+  );
+};
+
+export default InputButtonSet;
