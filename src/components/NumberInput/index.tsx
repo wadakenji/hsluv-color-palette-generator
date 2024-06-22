524738e9b6c3e5f8c8fbe95b0c267afc7ac85dbf
wadakenji <ponporopen@gmail.com>
2024-06-22 12:51:14 +0900

    feat: NumberInputコンポーネント作成

---
+++ src/components/NumberInput/index.tsx
@@ -0,0 +1,34 @@
+import { FC, HTMLAttributes, useEffect, useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+type Props = {
+  value: number;
+  onChange: (value: number) => void;
+} & Omit<HTMLAttributes<HTMLInputElement>, 'type' | 'value' | 'onChange'>;
+
+const NumberInput: FC<Props> = ({ value, onChange, className, ...props }) => {
+  const [textValue, setTextValue] = useState(String(value));
+
+  useEffect(() => {
+    if (textValue === '') onChange(0);
+
+    const numberValue = Number(textValue);
+    if (isNaN(numberValue)) return;
+
+    onChange(numberValue);
+  }, [textValue]);
+
+  return (
+    <>
+      <input
+        {...props}
+        type="number"
+        className={twMerge('p-2', className)}
+        value={value || textValue}
+        onChange={(e) => setTextValue(e.target.value)}
+      />
+    </>
+  );
+};
+
+export default NumberInput;
